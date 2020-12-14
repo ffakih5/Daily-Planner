bfe3a4e739d80b6df9b4e0e9186c6b5f8e433b82
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-14 13:38:54 +1100

    testing changes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // display current day/date//
-var todaysDate = moment().format('2020-12-14', 'YYYY-MM-DD');
-$("#currentDay").text("todaysDate");
+var todaysDate = moment().format('dddd, MMMM Do YYYY');
+$("#currentDay").html(todaysDate);
 
 //show time and description 
 $(document).ready(function () {
     $(".saveBtn").on("click", function() {
+
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         console.log("time");
+
+        localStorage.setItem(time, text)
+
     });
 
 
