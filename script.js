fcdea914f1f6c01676699bc6705f4de2f55e31eb
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-14 14:25:43 +1100

    fixed my js script link

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 // display current day/date//
 var todaysDate = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").html(todaysDate);
+console.log(todaysDate);
 
-//show time and description 
+//show time and description//
 $(document).ready(function () {
     $(".saveBtn").on("click", function() {
 
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        console.log("time");
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(time);
 
         localStorage.setItem(time, text)
     });
-    for (var i = 0; i < drinkList.length; i++) {
 
+    //function for displaying hours//
+
+    $.each(function(){
+
+    
 
 
 });
