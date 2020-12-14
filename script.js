2d3117e7ed0794fc0ec3ff25a5a050b3d6f4c11b
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-14 12:50:57 +1100

    adding time and text to js file

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 var currentDate = moment().format('2020-12-14', 'YYYY-MM-DD');
 $("#currentDay").text("currentDate");
 
+//show time and description 
+$(document).ready(function () {
+    $(".saveBtn").on("click", function() {
+        var time = $(this).attr("#hour");
+        var text = $(this).attr(".description").val();
+        
+    });
 
+
+
+});
