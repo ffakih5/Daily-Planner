86e678040919b6d4f9ca574ece83076fe70ef42f
Fatima Fakih <fatima.fakih@hotmail.com.au>
2020-12-17 16:41:51 +1100

    changed hours to 24 hr time format

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
-    var todaysDate = moment().format('MMMM Do YYYY');
-    $("#currentDay").text(todaysDate);
-        var thisHour = moment().hours();
+    //var todaysDate = moment().format('MMMM Do YYYY');
+    //$("#currentDay").text(todaysDate);
+        var currentHour = moment().hours();
 
     $(".description").each(function() {
-    var selectedHour = $(this). data("hour");
-    console.log(thisHour);
+    var selectedHour = $(this).data("hour");
+    console.log(currentHour);
     console.log(selectedHour);
     if(thisHour > selectedHour){
+        $(this).addClass("past");
 
 
     }
+
+    
          })
 
 
@@ -26,6 +29,7 @@ $(document).ready(function () {
 //show time and description//
 
     $(".saveBtn").on("click", function() {
+    
 
     var inputText = $(this).siblings(".description").val();
     var currentTime = $(this).parent().data("hour"); //can edit to .data (hour) and add a data-hour to 
@@ -46,6 +50,15 @@ $(document).ready(function () {
    //Get items
 
    $("#hour9 .description").val(localStorage.getItem("hour9"));
+   $("#hour10 .description").val(localStorage.getItem("hour10"));
+   $("#hour11 .description").val(localStorage.getItem("hour11"));
+   $("#hour12 .description").val(localStorage.getItem("hour12"));
+   $("#hour1 .description").val(localStorage.getItem("hour1"));
+   $("#hour2 .description").val(localStorage.getItem("hour2"));
+   $("#hour3 .description").val(localStorage.getItem("hour3"));
+   $("#hour4 .description").val(localStorage.getItem("hour4"));
+   $("#hour5 .description").val(localStorage.getItem("hour5"));
+  
 
     
 
